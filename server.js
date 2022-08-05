d958ad3cc4348d929a601ea04d860fb589d9e2f8
Sean M <sean.maddox89@yahoo.com>
2022-08-05 03:17:31 -0700

    moved order of app.use for public

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
