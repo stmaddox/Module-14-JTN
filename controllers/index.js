4f5de99a1b684aa3d5b1e8e0852e503cd06995ed
Sean M <sean.maddox89@yahoo.com>
2022-08-09 22:25:12 -0700

    fixed broken/missing code

--- controllers/index.js
+++ controllers/index.js
@@ -6,8 +6,4 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
-
 module.exports = router;
